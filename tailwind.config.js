7596f639394c4b4498b5ed508d6926198c9036b7
João Monteiro <monteiro_1998@hotmail.com>
2024-10-18 01:12:31 +0100

    Login Form

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{html,ts}"],
-  theme:
+  themes:
     {
     mytheme: {
       "primary": "#eb6f95",
@@ -15,13 +15,23 @@ module.exports = {
       "warning": "#fbbf24",
       "error": "#ef4444",
       "info": "#4a90e2",
+
+      "--rounded-box": "1rem", // border radius rounded-box utility class, used in card and other large boxes
+      "--rounded-btn": "0.5rem", // border radius rounded-btn utility class, used in buttons and similar element
+      "--rounded-badge": "1.9rem", // border radius rounded-badge utility class, used in badges and similar
+      "--animation-btn": "0.25s", // duration of animation when you click on button
+      "--animation-input": "0.2s", // duration of animation for inputs like checkbox, toggle, radio, etc
+      "--btn-focus-scale": "0.95", // scale transform of button when you focus on it
+      "--border-btn": "1px", // border width of buttons
+      "--tab-border": "1px", // border width of tabs
+      "--tab-radius": "0.5rem", // border radius of tabs
     },
     extend: {},
   },
   plugins: [require('daisyui'),],
   daisyui: {
     themes: false, // false: only light + dark | true: all themes | array: specific themes like this ["light", "dark", "cupcake"]
-    darkTheme: null, // name of one of the included themes for dark mode
+    darkTheme: false, // name of one of the included themes for dark mode
     base: true, // applies background color and foreground color for root element by default
     styled: true, // include daisyUI colors and design decisions for all components
     utils: true, // adds responsive and modifier utility classes
