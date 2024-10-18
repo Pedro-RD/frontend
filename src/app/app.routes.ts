0522db9d58fde565e288317ab57cc182e456903b
João Monteiro <157760466+monteiro1714@users.noreply.github.com>
2024-10-18 15:23:34 +0100

    Creation Pages Folder (#6)

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,5 +1,5 @@
 import { Routes } from '@angular/router';
-import {FormLoginComponent} from './components/form-login/form-login.component';
+import {FormLoginComponent} from './pages/form-login/form-login.component';
 
 export const routes: Routes = [
   {path:'login', component: FormLoginComponent}
