1010d3c7c2363552dce535b142df88c6b2fd354e
João Monteiro <157760466+monteiro1714@users.noreply.github.com>
2024-10-18 17:13:06 +0100

    Users Table Example (#7)

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import {FormLoginComponent} from './pages/form-login/form-login.component';
+import {UsersTableComponent} from './components/users-table/users-table.component';
 
 export const routes: Routes = [
-  {path:'login', component: FormLoginComponent}
+  {path:'', component: FormLoginComponent},
+  {path: 'users', component:UsersTableComponent}
 ];
