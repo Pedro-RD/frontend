c27a26dd3c7c3bba3a7ba09ed33a7f5924042c8f
Henrique <henrique.gomes.t0127541@edu.atec.pt>
2024-11-07 23:18:06 +0000

    MedicationTableSortFilterPaginator

--- src/app/components/old/medication-table/medication-table.component.ts
+++ src/app/components/old/medication-table/medication-table.component.ts
@@ -1,12 +1,45 @@
-import { Component } from '@angular/core';
+import { NgForOf } from '@angular/common';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { MedicationService } from '../../../services/medication/medication.service';
+import { MedicationTableRowComponent } from '../medication-table-row/medication-table-row.component';
 
 @Component({
   selector: 'app-medication-table',
   standalone: true,
-  imports: [],
+  imports: [NgForOf, MedicationTableRowComponent],
   templateUrl: './medication-table.component.html',
-  styleUrl: './medication-table.component.css'
+  styleUrls: ['./medication-table.component.css'],
 })
-export class MedicationTableComponent {
+export class MedicationTableComponent implements OnInit, OnDestroy {
+  subscriptions: Subscription[] = [];
+  medicamentos: any[] = [];
 
+  constructor(private medicationService: MedicationService) {}
+
+  ngOnInit() {
+    this.updateTable();
+  }
+
+  ngOnDestroy() {
+    this.subscriptions.forEach((subscription) => subscription.unsubscribe());
+    this.medicationService.clearAll(); // Caso exista um método para limpar dados no serviço
+  }
+
+  handleHeaderClick(col: string) {
+    this.medicationService.setOrderBy(col); // Implemente a lógica no serviço para definir a ordenação
+    this.updateTable();
+  }
+
+  updateTable() {
+    this.subscriptions.push(this.medicationService.fetchList().subscribe((data) => {
+      this.medicamentos = data;
+    }));
+  }
+
+  deleteMedication($event: number) {
+    this.subscriptions.push(this.medicationService.delete($event).subscribe(() => {
+      this.updateTable(); // Atualiza a tabela após exclusão
+    }));
+  }
 }
