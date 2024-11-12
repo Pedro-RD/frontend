5cab633b7b7b9ff101227bbf0f580fc1b0abf34b
João Monteiro <157760466+monteiro1714@users.noreply.github.com>
2024-11-12 18:34:25 +0000

    Perfil Residente Details (#21)

---
+++ src/app/pages/residents-create/residents-create.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-residents-create',
+  standalone: true,
+  imports: [],
+  templateUrl: './residents-create.component.html',
+  styleUrl: './residents-create.component.css'
+})
+export class ResidentsCreateComponent {
+
+}
---
+++ src/app/pages/residents-edit/residents-edit.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-residents-edit',
+  standalone: true,
+  imports: [],
+  templateUrl: './residents-edit.component.html',
+  styleUrl: './residents-edit.component.css'
+})
+export class ResidentsEditComponent {
+
+}
