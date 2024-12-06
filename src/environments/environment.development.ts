e9231d6549421c29ad00183130b918752c250f2a
Gustavo-M-Nogueira <161237672+Gustavo-M-Nogueira@users.noreply.github.com>
2024-12-06 00:15:00 +0000

    Resident photo update and delete (#54)
    * Resident photo update and delete
    
    * Probs Solved
    
    ---------
    
    Co-authored-by: João Monteiro <monteiro_1998@hotmail.com>

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,4 +1,6 @@
 export const environment = {
   production: false,
   apiUrl: 'https://larback.onrender.com/api/',
+  photoUser: 'https://larback.onrender.com/uploads/users/',
+  photoResident: 'https://larback.onrender.com/uploads/residents/',
 };
