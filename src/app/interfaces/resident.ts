6d91607b44d97ac9ed7ab4c73e9992309025c603
Pedro Dias <Pedro.Dias.T0098247@edu.atec.pt>
2024-12-09 21:42:44 +0000

    Add relative to resident on edit

--- src/app/interfaces/resident.ts
+++ src/app/interfaces/resident.ts
@@ -1,26 +1,24 @@
-import {CivilStatus} from './civil-status.enum';
-import {Diet} from './diet.enum';
+import { CivilStatus } from './civil-status.enum';
+import { Diet } from './diet.enum';
 import { Mobility } from './mobility.enum';
+import { User } from './user';
 
 export interface ResidentDTO {
-  name: string,
-  fiscalId: string,
-  birthDate: Date,
-  specificCare: string,
-  mobility: Mobility,
-  civilStatus: CivilStatus,
-  nationality: string,
-  diet: Diet,
-  dietRestrictions: string,
-  allergies: string,
-  bedNumber: number,
-  relatives: number[];
+  name: string;
+  fiscalId: string;
+  birthDate: Date;
+  specificCare: string;
+  mobility: Mobility;
+  civilStatus: CivilStatus;
+  nationality: string;
+  diet: Diet;
+  dietRestrictions: string;
+  allergies: string;
+  bedNumber: number;
+  relatives: number[] | User[];
   profilePicture?: string;
 }
 
 export interface Resident extends ResidentDTO {
-  id: number,
-
+  id: number;
 }
-
-
