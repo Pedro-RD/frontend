34e9bb3e0a528b64fe81ccf783cb9e36bbc8c0af
Pedro Dias <Pedro.Dias.T0098247@edu.atec.pt>
2024-12-10 21:38:50 +0000

    Add validation messages on click

--- src/app/components/form-health-report/form-health-report.component.ts
+++ src/app/components/form-health-report/form-health-report.component.ts
@@ -1,6 +1,11 @@
 import { Component, Input, Output, EventEmitter, OnInit } from '@angular/core';
 import { HealthReportDTO } from '../../interfaces/health-report';
-import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import {
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 import { environment } from '../../../environments/environment';
 import { InputComponent } from '../forms/input/input.component';
 import { ButtonComponent } from '../forms/button/button.component';
@@ -9,13 +14,9 @@ import { Location } from '@angular/common';
 @Component({
   selector: 'app-form-health-report',
   standalone: true,
-  imports: [
-    InputComponent,
-    ButtonComponent,
-    ReactiveFormsModule,
-  ],
+  imports: [InputComponent, ButtonComponent, ReactiveFormsModule],
   templateUrl: './form-health-report.component.html',
-  styleUrls: ['./form-health-report.component.css']
+  styleUrls: ['./form-health-report.component.css'],
 })
 export class FormHealthReportComponent implements OnInit {
   @Input() initialData: HealthReportDTO | undefined;
@@ -23,18 +24,23 @@ export class FormHealthReportComponent implements OnInit {
 
   constructor(private location: Location) {}
 
-
   arterialBloodPressure = new FormControl<string>('', [Validators.required]);
   temperature = new FormControl<number | null>(null, [Validators.required]);
   height = new FormControl<number | null>(null, [Validators.required]);
   weight = new FormControl<number | null>(null, [Validators.required]);
   respiratoryRate = new FormControl<number | null>(null, [Validators.required]);
   heartRate = new FormControl<number | null>(null, [Validators.required]);
-  bloodGlucoseLevel = new FormControl<number | null>(null, [Validators.required]);
+  bloodGlucoseLevel = new FormControl<number | null>(null, [
+    Validators.required,
+  ]);
   mobility = new FormControl<string>('', [Validators.required]);
   hydrationLevel = new FormControl<string>('', [Validators.required]);
-  cognitiveEmotionalAssessment = new FormControl<string>('', [Validators.required]);
-  bloodOxygenLevel = new FormControl<number | null>(null, [Validators.required]);
+  cognitiveEmotionalAssessment = new FormControl<string>('', [
+    Validators.required,
+  ]);
+  bloodOxygenLevel = new FormControl<number | null>(null, [
+    Validators.required,
+  ]);
 
   form: FormGroup = new FormGroup({
     arterialBloodPressure: this.arterialBloodPressure,
@@ -62,28 +68,39 @@ export class FormHealthReportComponent implements OnInit {
       this.bloodGlucoseLevel.setValue(data.bloodGlucoseLevel);
       this.mobility.setValue(data.mobility);
       this.hydrationLevel.setValue(data.hydrationLevel);
-      this.cognitiveEmotionalAssessment.setValue(data.cognitiveEmotionalAssessment);
+      this.cognitiveEmotionalAssessment.setValue(
+        data.cognitiveEmotionalAssessment,
+      );
       this.bloodOxygenLevel.setValue(data.bloodOxygenLevel);
     }
   }
 
   onSubmit() {
-    console.log('Form submitted:', this.form.value, this.form.valid, this.form.errors);
-    if (this.form.valid) {
-      this.createRequested.emit({
-        arterialBloodPressure: this.arterialBloodPressure.value!,
-        temperature: parseFloat(`${this.temperature.value!}`),
-        height: parseFloat(`${this.height.value}`),
-        weight: parseFloat(`${this.weight.value!}`),
-        respiratoryRate: parseInt(`${this.respiratoryRate.value!}`),
-        heartRate: parseInt(`${this.heartRate.value!}`),
-        bloodGlucoseLevel: parseInt(`${this.bloodGlucoseLevel.value!}`),
-        mobility: this.mobility.value!,
-        hydrationLevel: this.hydrationLevel.value!,
-        cognitiveEmotionalAssessment: this.cognitiveEmotionalAssessment.value!,
-        bloodOxygenLevel: parseFloat(`${this.bloodOxygenLevel.value!}`),
-      });
+    if (environment)
+      console.log(
+        'Form submitted:',
+        this.form.value,
+        this.form.valid,
+        this.form.errors,
+      );
+    if (this.form.invalid) {
+      this.form.markAllAsTouched();
+      return;
     }
+
+    this.createRequested.emit({
+      arterialBloodPressure: this.arterialBloodPressure.value!,
+      temperature: parseFloat(`${this.temperature.value!}`),
+      height: parseFloat(`${this.height.value}`),
+      weight: parseFloat(`${this.weight.value!}`),
+      respiratoryRate: parseInt(`${this.respiratoryRate.value!}`),
+      heartRate: parseInt(`${this.heartRate.value!}`),
+      bloodGlucoseLevel: parseInt(`${this.bloodGlucoseLevel.value!}`),
+      mobility: this.mobility.value!,
+      hydrationLevel: this.hydrationLevel.value!,
+      cognitiveEmotionalAssessment: this.cognitiveEmotionalAssessment.value!,
+      bloodOxygenLevel: parseFloat(`${this.bloodOxygenLevel.value!}`),
+    });
   }
 
   protected readonly environment = environment;
