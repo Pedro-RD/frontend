c9a1d23dd3f3c4caa8d4e4accca584760dd8538e
João Monteiro <monteiro_1998@hotmail.com>
2024-12-11 18:31:19 +0000

    Capacitor

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.example.app',
+  appName: 'frontend',
+  webDir: 'dist/frontend/browser',
+  android: {allowMixedContent:true, }
+};
+
+export default config;
